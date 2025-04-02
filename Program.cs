841ab3399d04658d4c0f4b650cf74734821a3f57
Dennys Kaykhe Justino de Assis <Dennyssenpai@gmail.com>
2025-04-02 14:31:32 -0300

    Eu amo calcular imc

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿decimal altura;
+decimal peso;
+decimal imc;
+
+Console.Clear();
+
+Console.WriteLine("--- CALCULO DE ICM ---");
+
+Console.Write("Altura (m)..: ");
+altura = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Peso (kg)...: ");
+peso = Convert.ToDecimal(Console.ReadLine());
+
+imc = peso / (altura * altura);
+
+Console.Write(@$"
+IMC: {imc} kg/m²");
